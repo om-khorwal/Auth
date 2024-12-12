0d95fe1317b94d68286481eeac9b2e7b146d3abb
om khorwal <amankhorwal250@gmail.com>
2024-12-12 22:28:55 +0530

    files imported and correction done for findOne

--- Backend/Routes/Authrouter.js
+++ Backend/Routes/Authrouter.js
@@ -1,24 +1,23 @@
-const { signup } = require('../Controllers/Authcontroller');
-const { signupvalidatation } = require('../Middlewares/Authvalidation');
-
 const router = require('express').Router();
+const UserModel = require('../Models/User');
+const bcrypt = require('bcrypt');
+
 router.post('/login', (req, res)=>{
 })
 
 router.post('/signup',async (req,res)=>{
     try{
         const {name,email,password}=req.body;
-        const user = await UserModel.findone({email});
+        const user = await UserModel.findOne({email});
         if(user){
             return res.status(409)
             .json({message:'User already present', success:false})
         }
-        UserModel = new UserModel({name,email,password});
-        UserModel.password = await bcrypt.hash(password,10);
-        await UserModel.save();
-        console.log(UserModel)
-        res.status(201)
-            .json({
+        const newUser = new UserModel({ name, email, password });
+        newUser.password = await bcrypt.hash(password, 10);
+        await newUser.save();
+        console.log(newUser)
+        res.status(201).json({
                 message:"signup successfully",
                 success:true
             })
